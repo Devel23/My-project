57a66692f3ed66effa82e7d6a775c689f51979de
DkS45 <mistik9880@gmail.com>
2026-04-02 15:14:34 +0300

    Popa

--- Assets/scripts/New/Enemy/BaseEnemy.cs
+++ Assets/scripts/New/Enemy/BaseEnemy.cs
@@ -1,7 +1,28 @@
+using Assets.Scripts.Entities;
 using UnityEngine;
 
 public class BaseEnemy : MonoBehaviour, IUnit
 {
+    [SerializeField] private HealthComponent m_health;
+    [SerializeField] private EnemyData m_enemyData;
+
+    private Player m_player;
+
+    private void OnEnable()
+    {
+        m_health.Died += Die;//popa
+    }
+
+    private void OnDisable()
+    {
+        m_health.Died -= Die;
+    }
+
+    public void Instanstate(Player player)
+    {
+        m_player = player;
+        m_health.Initialize(m_enemyData.Health);
+    }
     public void Action()
     {
         throw new System.NotImplementedException();
--- Assets/scripts/New/Enemy/EnemyData.cs
+++ Assets/scripts/New/Enemy/EnemyData.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public sealed class EnemyData : MonoBehaviour
 {
     [SerializeField] private int m_attackDamage;
+    [SerializeField][Min(0)] private float m_health;
 
     public int Damage => m_attackDamage;
+    public float Health => m_health;
 }
